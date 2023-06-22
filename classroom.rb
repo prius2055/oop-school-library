9cc14a255860a83dcea3ae55fe310f4d98c094a8
prius2055 <prius2055@gmail.com>
2023-06-22 09:57:53 +0100

    Update student.rb

--- classroom.rb
+++ classroom.rb
@@ -1,9 +1,10 @@
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
-    @student = []
+    @students = []
   end
 
   def add_student(student)
